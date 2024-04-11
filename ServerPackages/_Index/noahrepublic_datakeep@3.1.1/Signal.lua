e37a66526df0054f977116ac7b1aab23b750dc26
AyoWhat <necrogk75@gmail.com>
2024-04-11 21:37:28 +0900

    wally-package-types ServerPackages

--- ServerPackages/_Index/noahrepublic_datakeep@3.1.1/Signal.lua
+++ ServerPackages/_Index/noahrepublic_datakeep@3.1.1/Signal.lua
@@ -1,1 +1,4 @@
-return require(script.Parent.Parent["lucasmzreal_fastsignal@10.2.1"]["fastsignal"])
+local REQUIRED_MODULE = require(script.Parent.Parent["lucasmzreal_fastsignal@10.2.1"]["fastsignal"])
+export type Class = REQUIRED_MODULE.Class 
+export type ScriptConnection = REQUIRED_MODULE.ScriptConnection 
+return REQUIRED_MODULE
--- ServerPackages/datakeep.lua
+++ ServerPackages/datakeep.lua
@@ -1,1 +1,7 @@
-return require(script.Parent._Index["noahrepublic_datakeep@3.1.1"]["datakeep"])
+local REQUIRED_MODULE = require(script.Parent._Index["noahrepublic_datakeep@3.1.1"]["datakeep"])
+export type StoreInfo = REQUIRED_MODULE.StoreInfo 
+export type Promise = REQUIRED_MODULE.Promise 
+export type Store = REQUIRED_MODULE.Store 
+export type GlobalUpdates = REQUIRED_MODULE.GlobalUpdates 
+export type UnReleasedHandler = REQUIRED_MODULE.UnReleasedHandler 
+return REQUIRED_MODULE
