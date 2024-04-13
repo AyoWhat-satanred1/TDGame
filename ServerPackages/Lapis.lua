a9fcaba0d4b75a1c1a7f0076ec71907980f95fd0
AyoWhat <necrogk75@gmail.com>
2024-04-13 22:33:11 +0900

    wally-package-types Lapis

--- ServerPackages/Lapis.lua
+++ ServerPackages/Lapis.lua
@@ -1,1 +1,7 @@
-return require(script.Parent._Index["nezuo_lapis@0.2.11"]["lapis"])
+local REQUIRED_MODULE = require(script.Parent._Index["nezuo_lapis@0.2.11"]["lapis"])
+export type DataStoreService = REQUIRED_MODULE.DataStoreService 
+export type PartialLapisConfig = REQUIRED_MODULE.PartialLapisConfig 
+export type CollectionOptions<T> = REQUIRED_MODULE.CollectionOptions<T>
+export type Collection<T> = REQUIRED_MODULE.Collection<T>
+export type Document<T> = REQUIRED_MODULE.Document<T>
+return REQUIRED_MODULE
