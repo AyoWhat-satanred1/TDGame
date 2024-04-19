0a4eb3f844691b1e4f78b357298960d447b755a2
AyoWhat <necrogk75@gmail.com>
2024-04-19 23:15:27 +0900

    update datastoreService

--- Packages/KnitServer.lua
+++ Packages/KnitServer.lua
@@ -1,9 +1,1 @@
-export type ServiceDef = {
-    Name: string,
-    Client: { [any]: any },
-    KnitInit: (self: ServiceDef) -> (),
-    KnitStart: (self: ServiceDef) -> (),
-    [any]: any
-}
-
 return require(script.Parent._Index["sleitnick_knit@1.7.0"]["knit"]["KnitServer"])
